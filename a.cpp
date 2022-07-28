f426eb440ba22f3d9d983664cc3dd1e0e1ee71ad
YeJJ <412867710@qq.com>
2022-07-28 11:27:15 -0400

    蔚来笔试

--- a.cpp
+++ a.cpp
@@ -7,16 +7,47 @@
 
 using namespace std;
 
-int main() {
-    /*
-    if (!fork()) {
-        execv("help", NULL);
+// int main() {
+//     int n;
+//     cin>>n;
+//     string num = to_string(n);
+//     set<char> appeared;
+//     string ans;
+//     for (int i = num.size() - 1; i >= 0; i--) {
+//         if (appeared.count(num[i])) continue;
+//         ans += num[i];
+//         appeared.insert(num[i]);
+//     }
+//     cout<<ans<<endl;
+//     return 0;
+// }
+
+typedef unsigned long long ll;
+
+bool isValid(ll n) {
+    string num = to_string(n);
+    int i = 1;
+    int k = 0;
+    while (i < num.size()) {
+        if (num[i] != num[i - 1]) k++;
+        i++; 
     }
-    wait(NULL);
-    cout<<"im father process"<<endl;
-    */
-    int fd = open("makefile", O_RDWR);
-    cout<<fd<<endl;
-    while(1);
+    return k == 1;
+}
+
+int main() {
+    int n;
+    while (1) {
+        cin>>n;
+        if (n == 0) break;
+        int i = 1;
+        ll ans = -1;
+        while (ans == -1) {
+            ll cur = (ll) n * i;
+            if (isValid(cur)) ans = cur;
+            i++;
+        }
+        cout<<n<<": "<<ans<<endl;
+    }
     return 0;
 }
