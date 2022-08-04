c51f96bb2db1b96d155d117e53c78ec117f71806
YeJJ <412867710@qq.com>
2022-08-04 11:42:50 -0400

    ----

--- a.cpp
+++ a.cpp
@@ -7,35 +7,14 @@
 
 using namespace std;
 
-class Box {
+class A{
+    
+    void show(class* this) {
 
-public:
- int a;
- int b;
- int c;
-
- Box() {
-
- }
- Box(int a, int b, int c) {
-  this->a = a;
-  this->b = b;
-  this->c = c;
- }
-
- Box& operatro+(Box & aa, Box & bb) {
-    this->a = aa.a + bb.a;
-    this->b = aa.b + bb.b;
-    this->c = aa.c + bb.c;
-    return *this
- }
-
+    }
 };
 
 
 int main() {
-    Box a(1,1,1);
-    Box b(2,2,2);
-    auto c = a + b;
     return 0;
 }
